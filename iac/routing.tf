7f97b36828b9acca95ddb952d2feb58f05510e26
Gian-windr <gcamposa2@upao.edu.pe>
2026-05-05 20:34:07 -0500

    feat(network): definir vpc, subnets, gateways y rutas

---
+++ iac/routing.tf
@@ -0,0 +1,83 @@
+# ------------------------------------------------------------------------------
+# ROUTE TABLES & ASSOCIATIONS
+# ------------------------------------------------------------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = { Name = "rt-public-${var.project_name}-${terraform.workspace}" }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+  tags = { Name = "rt-private-a-${var.project_name}-${terraform.workspace}" }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+  tags = { Name = "rt-private-b-${var.project_name}-${terraform.workspace}" }
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+# ------------------------------------------------------------------------------
+# VPC ENDPOINTS & SECURITY
+# ------------------------------------------------------------------------------
+resource "aws_security_group" "lambda" {
+  name        = "sg-lambda-${var.project_name}-${terraform.workspace}"
+  description = "Security group for Lambda functions"
+  vpc_id      = aws_vpc.main.id
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "sg-lambda-${var.project_name}-${terraform.workspace}" }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private_a.id, aws_route_table.private_b.id]
+  tags              = { Name = "vpce-s3-${var.project_name}-${terraform.workspace}" }
+}
+
+resource "aws_vpc_endpoint" "sqs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.sqs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  security_group_ids  = [aws_security_group.lambda.id]
+  tags                = { Name = "vpce-sqs-${var.project_name}-${terraform.workspace}" }
+}
